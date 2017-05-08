06caa807b33d975b4ccd27f72254ae7a45001259
PaulArthur Jeantaud <jeantaud@gmail.com>
2017-05-08 17:32:28 +0200

    My proposal

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix : creates a matrix which is able to store its Inverse
+# so that it can be computed once for all, and then used as many times as needed 
 
-## Write a short comment describing this function
+
+## Using those two functions together unable us to manipulate matrix whose inverse won't be computed more than once
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(invers) i <<- invers
+        getinverse <- function() i
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve : adaptation of the function 'solve' to our new type of matrix 'makeCacheMatrix'
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinverse(i)
+        i
 }
